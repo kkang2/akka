5927058bc8abe73fc76762961c31c970b73d341b
bhkim <bhkim@mobigen.com>
2018-02-20 17:46:30 +0900

    akka java init commit

---
+++ akka_http_java/src/main/java/com/psj/akka/domain/Alarm.java
@@ -0,0 +1,24 @@
+package com.psj.akka.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Alarm {
+	final String user;
+	final String message;
+	
+	@JsonCreator
+	Alarm(@JsonProperty("user") String user,
+         @JsonProperty("message") String message) {
+      this.user = user;
+      this.message = message;
+    }
+
+	public String getUser() {
+		return user;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
