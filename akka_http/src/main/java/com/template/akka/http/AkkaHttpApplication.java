8c4aed3e135529d611a6f3908503b3725bd17c1d
sanghya <sanghya@gmail.com>
2020-08-26 18:59:11 +0900

    새로운 버전의 akka http 가이드 코드 프로젝트

---
+++ akka_http/src/main/java/com/template/akka/http/AkkaHttpApplication.java
@@ -0,0 +1,13 @@
+package com.template.akka.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkaHttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkkaHttpApplication.class, args);
+    }
+
+}
