02796106d8907a04e07bb077ac37607fedc1418d
kkang2 <mallakkang@gmail.com>
2020-11-12 16:34:15 +0900

    Add files via upload
    Akka Actors 관련 샘플 작성 용 프로젝트 커밋

---
+++ actors/src/main/java/com/example/actors/ActorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.actors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActorsApplication.class, args);
+    }
+
+}
